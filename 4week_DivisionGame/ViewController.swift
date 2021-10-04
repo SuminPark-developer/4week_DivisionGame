fa4a387ec67257365c4dad974371c6670df4e15c
SuminPark-developer <float@kakao.com>
2021-10-04 18:58:00 +0900

    Initial Commit

---
+++ 4week_DivisionGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4week_DivisionGame
+//
+//  Created by sumin on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
