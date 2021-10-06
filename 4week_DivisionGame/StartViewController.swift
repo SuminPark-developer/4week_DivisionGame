de2158c53a7259bf2fd74b7b80ddb88251023ef4
SuminPark-developer <float@kakao.com>
2021-10-06 17:42:20 +0900

    앱 아이콘 추가, 런치스크린 화면 추가, 난이도 추가(Easy Mode, Hard Mode)

--- 4week_DivisionGame/StartViewController.swift
+++ 4week_DivisionGame/StartViewController.swift
@@ -9,29 +9,57 @@ import UIKit
 
 class StartViewController: UIViewController {
 
-    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var easyButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    
+    @IBOutlet weak var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        startButton.layer.cornerRadius = 10
-        startButton.layer.borderWidth = 1
-        startButton.layer.borderColor = UIColor.gray.cgColor
+        easyButton.layer.cornerRadius = 10
+        easyButton.layer.borderWidth = 1
+        easyButton.layer.borderColor = UIColor.gray.cgColor
+        
+        hardButton.layer.cornerRadius = 10
+        hardButton.layer.borderWidth = 1
+        hardButton.layer.borderColor = UIColor.gray.cgColor
     }
     
     // 참고 : https://faith-developer.tistory.com/31
     // StartVC에선 네비게이션바가 안보이게 하고,
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        
+        // 애니메이션 - https://velog.io/@kerri/iOS-Swift-Bounce-Animation-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-g1wzu4ur
+        stackView.center.x = self.view.frame.width + 30
+        UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 30, options: [], animations: {
+            self.stackView.center.x = self.view.frame.width / 2
+        }, completion: nil)
     }
     // gameVC에선 네비게이션바가 보이게 설정.
     override func viewWillDisappear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
-    @IBAction func ClickStartButton(_ sender: UIButton) {
-        let gameVC = (self.storyboard?.instantiateViewController(withIdentifier: "MainVC"))!
+    @IBAction func clickEasyButton(_ sender: UIButton) {
+        let gameVC = (self.storyboard?.instantiateViewController(withIdentifier: "MainVC"))! as! ViewController
+        gameVC.mode = "Easy"
         self.navigationController?.pushViewController(gameVC, animated: true)
     }
+    
+    @IBAction func clickHardMode(_ sender: UIButton) {
+        let gameVC = (self.storyboard?.instantiateViewController(withIdentifier: "MainVC"))! as! ViewController
+        gameVC.mode = "Hard"
+        self.navigationController?.pushViewController(gameVC, animated: true)
+    }
+    
+    // segue 쓸 필요 없을 듯.
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "hardMode" {
+//            let modeDetail = segue.destination as? ViewController
+//            modeDetail!.mode = "hardMode"
+//        }
+//    }
     
 }
--- 4week_DivisionGame/ViewController.swift
+++ 4week_DivisionGame/ViewController.swift
@@ -11,8 +11,11 @@ import AVFoundation
 var soundEffect: AVAudioPlayer?
 
 class ViewController: UIViewController {
+    var mode: String? // Easy Mode or Hard Mode
 
-    var imageArray: [UIImage] = [UIImage(named: "blueSheep.png")!, UIImage(named: "redStar.png")!]
+    var imageArray: [UIImage] = [UIImage(named: "blueSheep.png")!, UIImage(named: "redStar.png")!, UIImage(named: "mintChicken.png")!, UIImage(named: "yellowPoodle.png")!]
+    var imageEasyArray: [UIImage] = [UIImage(named: "blueSheep.png")!, UIImage(named: "redStar.png")!]
+    var imageHardArray: [UIImage] = [UIImage(named: "mintChicken.png")!, UIImage(named: "yellowPoodle.png")!]
     var imageArrayCopy: [UIImage]!
     
     var countTimer: Timer?
@@ -24,7 +27,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreUI: UILabel! // 우측 상단 점수표시 uiLabel
     
     @IBOutlet weak var leftStandard: UIImageView! // 왼쪽 판단 기준
+    @IBOutlet weak var leftStandard2: UIImageView! // 왼쪽 판단 기준(하드모드 추가)
     @IBOutlet weak var rightStandard: UIImageView! // 오른쪽 판단 기준
+    @IBOutlet weak var rightStandard2: UIImageView! // 오른쪽 판단 기준(하드모드 추가)
     
     @IBOutlet weak var queueElement1: UIImageView!
     @IBOutlet weak var queueElement2: UIImageView!
@@ -38,10 +43,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         playBackgroundSound() // 음악 함수 실행
         
-        setStandard() // 좌,우측 기준 설정 함수
+        leftButton.layer.cornerRadius = 10
+        leftButton.layer.borderWidth = 1
+        leftButton.layer.borderColor = UIColor.gray.cgColor
+        
+        rightButton.layer.cornerRadius = 10
+        rightButton.layer.borderWidth = 1
+        rightButton.layer.borderColor = UIColor.gray.cgColor
+        
+        leftStandard.layer.cornerRadius = 10
+        leftStandard2.layer.cornerRadius = 10
+        rightStandard.layer.cornerRadius = 10
+        rightStandard2.layer.cornerRadius = 10
+        
+        queueElement1.layer.cornerRadius = 10
+        queueElement2.layer.cornerRadius = 10
+        queueElement3.layer.cornerRadius = 10
+        queueElement4.layer.cornerRadius = 10
+        queueElement5.layer.cornerRadius = 10
+        
+        if mode == "Hard" {
+            setEasyStandard() // 좌,우측 이지모드 기준 설정 함수
+            setHardStandard() // 좌, 우측 하드모드 기준 설정 함수
+        }
+        else if mode == "Easy" {
+            setEasyStandard() // 좌,우측 이지모드 기준 설정 함수
+            leftStandard2.removeFromSuperview() // 하드모드 이미지뷰 삭제
+            rightStandard2.removeFromSuperview() // 하드모드 이미지뷰 삭제
+        }
         
         self.countTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(changeTimerText), userInfo: nil, repeats: true) // 타이머 반복 실행
         
@@ -49,51 +80,107 @@ class ViewController: UIViewController {
 
     // MARK: - 왼쪽 버튼 클릭 : 좌측 하단
     @IBAction func clickLeftButton(_ sender: UIButton) {
-        checkSame("left") // 기준과 클릭한 버튼이 동일한지 체크.
+        if mode == "Easy" {
+            checkSame("left", "Easy") // 기준과 클릭한 버튼이 동일한지 체크.
+        }
+        else if mode == "Hard" {
+            checkSame("left", "Hard") // 기준과 클릭한 버튼이 동일한지 체크.
+        }
     }
     
     // MARK: - 오른쪽 버튼 클릭 : 우측 하단
     @IBAction func clickRightButton(_ sender: UIButton) {
-        checkSame("right") // 기준과 클릭한 버튼이 동일한지 체크.
+        if mode == "Easy" {
+            checkSame("right", "Easy") // 기준과 클릭한 버튼이 동일한지 체크.
+        }
+        else if mode == "Hard" {
+            checkSame("right", "Hard") // 기준과 클릭한 버튼이 동일한지 체크.
+        }
     }
     
     // MARK: - 기준과 동일한 버튼을 눌렀는지 체크하는 함수.
-    func checkSame(_ direction: String) {
-        // 왼쪽 버튼 체크.
-        if direction == "left" {
-            if leftStandard.image?.pngData() == self.queueElement1.image?.pngData() { // 좌측 기준과 맨밑이미지가 같다면,
-                score += 100 // 득점.
-                changeQueueImage()
-                changeScoreText()
-                print("점수는 \(score)점")
+    func checkSame(_ direction: String, _ mode: String) {
+        
+        if mode == "Easy" { // 이지 모드
+            if direction == "left" { // (이지모드) 왼쪽 버튼 체크.
+                if leftStandard.image?.pngData() == self.queueElement1.image?.pngData() { // 좌측 기준과 맨밑이미지가 같다면,
+                    score += 100 // 득점.
+                    changeEasyQueueImage()
+                    changeScoreText()
+                    print("점수는 \(score)점")
+                }
+                else { // 틀렸다면 최종 결과로 이동.(score 전달해야함.)
+                    endEffect() // 음악 정지, 타이머 종료(메모리 해제)
+                    let finishVC = (self.storyboard?.instantiateViewController(withIdentifier: "FinishVC")) as! FinishViewController
+                    finishVC.text = "\(String(score + (60 - counter)))점" // 점수 전달
+                    self.navigationController?.pushViewController(finishVC, animated: true)
+                }
             }
-            else { // 틀렸다면 최종 결과로 이동.(score 전달해야함.)
-                endEffect() // 음악 정지, 타이머 종료(메모리 해제)
-                let finishVC = (self.storyboard?.instantiateViewController(withIdentifier: "FinishVC")) as! FinishViewController
-                finishVC.text = "\(String(score + (60 - counter)))점" // 점수 전달
-                self.navigationController?.pushViewController(finishVC, animated: true)
+            else if direction == "right" { // (이지모드) 우측 버튼 체크.
+                if rightStandard.image?.pngData() == queueElement1.image?.pngData() { // 우측 기준과 맨밑이미지가 같다면,
+                    score += 100 // 득점.
+                    changeEasyQueueImage()
+                    changeScoreText()
+                    print("점수는 \(score)점")
+                }
+                else { // 틀렸다면 최종 결과로 이동.(점수 전달해야함.)
+                    endEffect() // 음악 정지, 타이머 종료(메모리 해제)
+                    let finishVC = (self.storyboard?.instantiateViewController(withIdentifier: "FinishVC")) as! FinishViewController
+                    finishVC.text = "\(String(score + (60 - counter)))점" // 점수 전달
+                    self.navigationController?.pushViewController(finishVC, animated: true)
+                }
             }
         }
-        // 우측 버튼 체크.
-        else if direction == "right" {
-            if rightStandard.image?.pngData() == queueElement1.image?.pngData() { // 우측 기준과 맨밑이미지가 같다면,
-                score += 100 // 득점.
-                changeQueueImage()
-                changeScoreText()
-                print("점수는 \(score)점")
+        else if mode == "Hard" { // 하드 모드
+            if direction == "left" { // (하드모드) 왼쪽 버튼 체크.
+                if (leftStandard.image?.pngData() == self.queueElement1.image?.pngData()) || (leftStandard2.image?.pngData() == self.queueElement1.image?.pngData()) { // 좌측 기준1,2와 맨밑이미지가 같다면,
+                    score += 150 // 득점.
+                    changeHardQueueImage()
+                    changeScoreText()
+                    print("점수는 \(score)점")
+                }
+                else { // 틀렸다면 최종 결과로 이동.(score 전달해야함.)
+                    endEffect() // 음악 정지, 타이머 종료(메모리 해제)
+                    let finishVC = (self.storyboard?.instantiateViewController(withIdentifier: "FinishVC")) as! FinishViewController
+                    finishVC.text = "\(String(score + (60 - counter)))점" // 점수 전달
+                    self.navigationController?.pushViewController(finishVC, animated: true)
+                }
             }
-            else { // 틀렸다면 최종 결과로 이동.(점수 전달해야함.)
-                endEffect() // 음악 정지, 타이머 종료(메모리 해제)
-                let finishVC = (self.storyboard?.instantiateViewController(withIdentifier: "FinishVC")) as! FinishViewController
-                finishVC.text = "\(String(score + (60 - counter)))점" // 점수 전달
-                self.navigationController?.pushViewController(finishVC, animated: true)
+            else if direction == "right" { // (하드모드) 우측 버튼 체크.
+                if ((rightStandard.image?.pngData() == self.queueElement1.image?.pngData())) || (rightStandard2.image?.pngData() == queueElement1.image?.pngData()) { // 우측 기준1,2와 맨밑이미지가 같다면,
+                    score += 150 // 득점.
+                    changeHardQueueImage()
+                    changeScoreText()
+                    print("점수는 \(score)점")
+                }
+                else { // 틀렸다면 최종 결과로 이동.(점수 전달해야함.)
+                    endEffect() // 음악 정지, 타이머 종료(메모리 해제)
+                    let finishVC = (self.storyboard?.instantiateViewController(withIdentifier: "FinishVC")) as! FinishViewController
+                    finishVC.text = "\(String(score + (60 - counter)))점" // 점수 전달
+                    self.navigationController?.pushViewController(finishVC, animated: true)
+                }
             }
         }
         
+        
+        
     }
     
-    // MARK: - queue 이미지 변경 함수.
-    func changeQueueImage() {
+    // MARK: - 이지모드 queue 이미지 변경 함수.
+    func changeEasyQueueImage() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            DispatchQueue.main.async {
+                self.queueElement1.image = self.queueElement2.image
+                self.queueElement2.image = self.queueElement3.image
+                self.queueElement3.image = self.queueElement4.image
+                self.queueElement4.image = self.queueElement5.image
+                self.queueElement5.image = self.imageEasyArray.randomElement()
+            }
+        }
+    }
+    
+    // MARK: - 하드모드 queue 이미지 변경 함수.
+    func changeHardQueueImage() {
         DispatchQueue.global(qos: .userInitiated).async {
             DispatchQueue.main.async {
                 self.queueElement1.image = self.queueElement2.image
@@ -131,9 +218,9 @@ class ViewController: UIViewController {
         }
     }
     
-    // MARK: - 좌,우측 기준 이미지 설정 함수.
-    func setStandard() {
-        imageArrayCopy = imageArray // 카피 어레이에 복사.
+    // MARK: - 좌,우측 이지모드 기준 이미지 설정 함수.
+    func setEasyStandard() {
+        imageArrayCopy = imageEasyArray // 카피 어레이에 복사.
         leftStandard.image = imageArrayCopy?.randomElement()
         
         for i in 0..<imageArrayCopy.count { // 기준은 이미지당 하나씩 - 이미 사용된 이미지는 삭제.
@@ -144,6 +231,20 @@ class ViewController: UIViewController {
         }
         rightStandard.image = imageArrayCopy.randomElement() // 다른 기준에 새로운 이미지 할당.
         
+    }
+    
+    // MARK: - 좌,우측 하드모드 기준 이미지 설정 함수.
+    func setHardStandard() {
+        imageArrayCopy = imageHardArray // 카피 어레이에 복사.
+        leftStandard2.image = imageArrayCopy?.randomElement()
+        
+        for i in 0..<imageArrayCopy.count { // 기준은 이미지당 하나씩 - 이미 사용된 이미지는 삭제.
+            if leftStandard2.image == imageArrayCopy[i] {
+                imageArrayCopy.remove(at: i)
+                break
+            }
+        }
+        rightStandard2.image = imageArrayCopy.randomElement() // 다른 기준에 새로운 이미지 할당.
     }
 
     // MARK: - 음악 실행 함수.
